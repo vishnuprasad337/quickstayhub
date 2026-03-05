b272e7fec71e5beb813b6267bd3116c2b5a59782
vishnu prasad <mvihnu673@gmail.com>
2026-03-05 11:35:21 +0530

    add hosts in it

--- quickstayhub/settings.py
+++ quickstayhub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5sydse$v*ne^ss=__406_im3v+i74hv8jc=ge=@jdzh2abet8)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
